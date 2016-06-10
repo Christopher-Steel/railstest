fc8862fb1653a0adce8deae037f7d0d70d5567c2
Christopher-Steel <christopher.g.steel@gmail.com>
2016-06-10 17:15:55 +0100

    Added proper page size selector in ui, created model for favourites

--- app/controllers/fbn_list_controller.rb
+++ app/controllers/fbn_list_controller.rb
@@ -4,13 +4,13 @@ class FbnListController < ApplicationController
     limit: 100
   }
 
-  def sanitize_integer(data, default)
+  private def sanitize_integer(data, default)
     data.nil? ? default : Integer(data)
   rescue ArgumentError
     default
   end
 
-  def sanitize_params
+  private def sanitize_params
     [:offset, :limit].each do |pm|
       params[pm] = sanitize_integer(params[pm], DEFAULT_PARAMS[pm])
     end
@@ -18,17 +18,18 @@ class FbnListController < ApplicationController
 
   def show
     sanitize_params
-    offset = params[:offset]
-    limit = params[:limit]
-    last = offset + limit
+    @offset = params[:offset]
+    @limit = params[:limit]
+    last = @offset + @limit
 
-    @list = FizzBuzzNumber.range(offset...last).map(&:to_s)
-    @prev_page_link = url_for(offset: offset - limit, limit: limit)
-    @next_page_link = url_for(offset: offset + limit, limit: limit)
+    @numbers = FizzBuzzNumber.range(@offset...last).map(&:to_s)
+    favourites = Favourite.where(number: @offset...last)
+    @prev_page_link = url_for(offset: @offset - @limit, limit: @limit)
+    @next_page_link = url_for(offset: @offset + @limit, limit: @limit)
 
     respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @list }
+      format.html
+      format.json { render json: @numbers }
     end
 
   end
